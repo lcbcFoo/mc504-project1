bdf9dffd7d10c5b7dff75216e71e60d7a08c191e
Foo <lcbc.lucascastro@gmail.com>
2017-04-13 13:31:49 -0300

    Threads implemented

---
+++ threads.c
@@ -0,0 +1,66 @@
+/* Implementation of the threads-related functions */
+#include "threads.h"
+#include "graphics.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+
+void think(int student_id){
+    sleep(sleeping_time[student_id]);
+}
+
+void check_pens(int student_id){
+    if(states[student_id] == D && states[LEFT] != W && states[RIGHT] != W){
+        states[student_id] = W;
+        display();
+        sem_post(&students[student_id]);
+    }
+}
+
+void pick_pens(int student_id){
+    sem_wait(&semaphore);
+    states[student_id] = D;
+    display();
+    check_pens(student_id);
+    sem_post(&semaphore);
+    sem_wait(&students[student_id]);
+}
+
+void writing(int student_id){
+    sem_wait(&sem_questions);
+    questions[student_id]++;
+    total_questions++;
+
+    if(total_questions == MAX)
+        final_layout();
+
+    sem_post(&sem_questions);
+
+    sleep(sleeping_time[student_id]);
+}
+
+void release_pens(int student_id){
+    sem_wait(&semaphore);
+    states[student_id] = T;
+    display();
+    check_pens(LEFT);
+    check_pens(RIGHT);
+    sem_post(&semaphore);
+}
+
+void* student_function(void* v){
+    int student_id = *(int *) v;
+
+    sleep(start_delay[student_id]);
+
+    while(1){
+        pick_pens(student_id);
+        writing(student_id);
+        release_pens(student_id);
+        think(student_id);
+    }
+}
---
+++ threads.h
@@ -0,0 +1,52 @@
+/* Header for the  threads-related functions */
+
+/* The algorithm used in the threads implementation is based on the code
+ * examples given in class by prof. Islene Calciolari Garcia and on the
+ * examples presented by the book "Modern Operating Systems", 4th edition,
+ * Pearson, 2014, written by A. S. Tanenbaum and H. Bos
+ */
+
+#include <pthread.h>
+#include <semaphore.h>
+
+/* define number of students and max total writings */
+#define N 5
+#define MAX 10
+
+#define RIGHT (student_id + 1) % N
+#define LEFT (student_id + N - 1) % N
+
+typedef enum {T, D, W} state_t;
+
+/* Current state of each student */
+state_t states[N];
+
+/* Semaphore for picking a pen*/
+sem_t semaphore;
+
+/* Semaphore for holding pens for each student */
+sem_t students[N];
+
+/* Semaphore for incrementing total questions */
+sem_t sem_questions;
+
+/* Counters */
+int questions[N];
+int total_questions;
+
+/* Specify how long every student must sleep depending on the animation mode */
+int sleeping_time[N];
+int start_delay[N];
+
+
+void think(int student_id);
+
+void check_pens(int student_id);
+
+void pick_pens(int student_id);
+
+void writing(int student_id);
+
+void release_pens(int student_id);
+
+void* student_function(void* v);
